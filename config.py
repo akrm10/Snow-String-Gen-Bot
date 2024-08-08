75742c600479cf641e3d07243321c5d555ef1aba
akrm10 <akrm54391127@gmail.com>
2024-08-08 17:45:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("21209802"))
+API_HASH = getenv("eed1c8387c6ee80009527e07d9d58cc0")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("6807629743:AAHsnFL_eb9F-egK2yjvvxVgKWIcQIhpNDA")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
+OWNER_ID = int(getenv("OWNER_ID", 6716174264))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Kdramaland_Official")
